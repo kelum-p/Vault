a262c5c0e42e96b9a449df34fb618e3a3befd0f0
Kelum <kelum.peiris@polarmobile.com>
2011-02-05 21:44:05 -0500

    First Commit

---
+++ src/CocoB/Vault/UI/Framework/VaultBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * VaultBootstrapper.cs
+ * 
+ * Author: Kelum Peiris (kelum.peiris@polarmobile.com)
+ * Date: 2/5/2011 9:32:08 PM
+ *
+ */
+
+using Caliburn.Micro;
+using CocoB.Vault.UI.Shell;
+
+namespace CocoB.Vault.UI.Framework
+{
+    public class VaultBootstrapper : Bootstrapper<ShellViewModel>
+    {
+    }
+}
---
+++ src/CocoB/Vault/UI/Shell/ShellViewModel.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * ShellViewModel.cs
+ * 
+ * Author: Kelum Peiris (kelum.peiris@polarmobile.com)
+ * Date: 2/5/2011 9:36:00 PM
+ *
+ */
+
+using System.Windows;
+using Caliburn.Micro;
+
+namespace CocoB.Vault.UI.Shell
+{
+    public class ShellViewModel : PropertyChangedBase
+    {
+        string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public bool CanSayHello
+        {
+            get { return !string.IsNullOrWhiteSpace(Name); }
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show(string.Format("Hello {0}!", Name)); //Don't do this in real life :)
+        }
+    }
+}
